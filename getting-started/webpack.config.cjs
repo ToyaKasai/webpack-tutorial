524a4efcb8facb607546ce97b0105be0af627080
ToyaKasai <kasaitoya@team-lab.com>
2024-09-18 16:49:33 +0900

    setting webpack config

---
+++ getting-started/webpack.config.cjs
@@ -0,0 +1,15 @@
+// nodeに組み込まれているモジュール
+const path = require("path");
+
+module.exports = {
+  /** モード */
+  mode: "development",
+  /** エントリーポイント */
+  entry: ".src/app.cjs",
+  /** 出力設定 */
+  output: {
+    // __dirname + public のように書くと、OS によってエラーが出る
+    path: path.resolve(__dirname, "public"),
+    filename: "js/bundle.js",
+  },
+};
