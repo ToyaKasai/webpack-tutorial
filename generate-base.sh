59b26dbfa04b4ed862294e9b93c73b39cbe5cccc
ToyaKasai <kasaitoya@team-lab.com>
2024-09-23 04:32:54 +0900

    add generate-base.sh

---
+++ generate-base.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create 'public' directory and 'index.html' file
+mkdir -p public
+touch public/index.html
+
+# Create 'src/js' directory and 'app.js' file
+mkdir -p src/js
+touch src/js/app.js
+
+# Create 'webpack.config.cjs' file
+touch webpack.config.cjs
